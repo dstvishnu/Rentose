d3fcd5fe1079ab718be9496502e1556ff6620a2d
Vishnu <dstvishnu@gmail.com>
2019-04-04 23:19:57 +0530

    Initial Commit

---
+++ src/main/java/com/rentose/rentose/RentoseApplication.java
@@ -0,0 +1,13 @@
+package com.rentose.rentose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentoseApplication.class, args);
+	}
+
+}
